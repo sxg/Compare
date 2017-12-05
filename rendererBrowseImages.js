66d977fbf19f2c3dddef9f51039da54023aaea15
Satyam Ghodasara <sghodas@gmail.com>
2017-12-05 16:57:23 -0500

    Added image folder selection with error handling

--- rendererBrowseImages.js
+++ rendererBrowseImages.js
@@ -5,6 +5,9 @@
 const {remote} = require('electron')
 const {dialog} = remote
 
+const _ = require('lodash')
+const path = require('path')
+
 /// View
 // Window
 const window = remote.getCurrentWindow()
@@ -12,11 +15,31 @@ const window = remote.getCurrentWindow()
 // Button
 const imagesButton = document.getElementById('button-images')
 
+// Input container
+const imagesContainer = document.getElementById('container-images')
+
 /// UI Actions
 // Browse folder with images
 imagesButton.addEventListener('click', event => {
+  hideErrors()
   dialog.showOpenDialog(window, {properties: ['openDirectory']},
-  filePaths => {
-
-  })
+    filePaths => {
+      if (filePaths) {
+        // Remove all non .png files
+        _.remove(filePaths, filePath => {
+          return path.extname(filePath) !== '.png'
+        })
+      } else {
+        showError(imagesContainer)
+      }
+    })
 })
+
+/// Helpers
+const showError = function (container) {
+  container.classList.add('error')
+}
+
+const hideErrors = function() {
+  imagesContainer.classList.remove('error')
+}
