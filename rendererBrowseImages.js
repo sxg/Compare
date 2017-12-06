062b01a078e6822be057c2317829646cd3471358
Satyam Ghodasara <sghodas@gmail.com>
2017-12-05 23:52:48 -0500

    Added comments to main window renderer

--- rendererBrowseImages.js
+++ rendererBrowseImages.js
@@ -1,10 +1,8 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
+// Import Electron components
 const {remote} = require('electron')
 const {dialog, BrowserWindow} = remote
 
+// Import Node dependencies
 const _ = require('lodash')
 const path = require('path')
 const fs = require('fs')
