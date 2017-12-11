08af25719ee6cbd72d82f0ff047d7294e4526692
Satyam Ghodasara <sghodas@gmail.com>
2017-12-11 12:55:56 -0500

    Added name field on main.html

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 400, height: 250})
+  mainWindow = new BrowserWindow({width: 400, height: 375})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
