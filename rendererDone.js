f4b80a618390c5d184aa218eb57a80dbaa433015
Satyam Ghodasara <sghodas@gmail.com>
2017-12-12 08:59:47 -0500

    Added keybinding for done button

--- rendererDone.js
+++ rendererDone.js
@@ -2,11 +2,19 @@
 // Electron components
 const {app} = require('electron').remote
 
+// Node dependencies
+const Mousetrap = require('mousetrap')
+
 /// Model
 const doneButton = document.getElementById('button-done')
 
 /// UI Actions
 // Done button
 doneButton.addEventListener('click', event => {
+  app.quit()
+})
+
+// Keybindings
+Mousetrap.bind('enter', event => {
   app.quit()
 })
